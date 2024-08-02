df190fe93ddcd71040edf0d8a7aba866e5b91c7a
Bugra Eryilmaz <bugra.eryilmaz@epfl.ch>
2024-08-02 18:23:15 +0200

    Check the program folder

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ The extension assumes the following:
 - The `Makefile` has the following targets:
   - `build_{BINARY_NAME}`: builds the assembly or verilog code
   - `clean`: cleans the build directory
-- After the build, there should be an executable called `Vtb` in the root folder of the current workspace. For assembly assignments, we will provide the `Vtb` executable from the CPU we have written in verilog. For verilog assignments, the makefile should generate the `Vtb` executable. This executable should implement the debug adapter protocol.
+- After the build, there should be an executable called `Vtb` in the same folder as the file the student presses debug button on. For assembly assignments, we will provide the `Vtb` executable from the CPU we have written in verilog. For verilog assignments, the makefile should generate the `Vtb` executable. This executable should implement the debug adapter protocol.
 - After the build, if the compiled verilog supports `mem_init` functionality, there should be a binary file called `{BINARY_NAME}.bin` and an executable that is compiled with debug symbols and called `{BINARY_NAME}` in the `out` directory.
 
 ## Extension Settings
@@ -31,6 +31,6 @@ If you wncounter any issues, please report them on the GitHub page of the extens
 
 ## Release Notes
 
-### 0.0.1
+### 0.1.0
 
 Initial release of cs200 extension.
--- src/extension.ts
+++ src/extension.ts
@@ -1,39 +1,37 @@
 import * as vscode from "vscode";
 import { WebViewPanel } from "./WebView";
 import { activateMockDebug } from "./activateMockDebug";
-import { ProviderResult } from "vscode";
+import { existsSync } from "fs";
 import * as cp from "child_process";
 
-const runMode: "external" | "server" | "namedPipeServer" | "inline" = "inline";
-
-let cs200Panel: vscode.WebviewPanel;
+let output = vscode.window.createOutputChannel("CS200");
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "cs200" is now active!');
+  output.appendLine('Congratulations, your extension "cs200" is now active!');
 
   vscode.debug.onDidStartDebugSession((e) => {
-    console.log("Debug session started");
+    output.appendLine("Debug session started");
     vscode.commands.executeCommand("cs200.helloWorld");
     WebViewPanel.clearCallbacks();
     WebViewPanel.addCallback((message: any) => {
       if (message.command === "updateInput") {
-        console.log("Sending message: ", message);
+        output.appendLine("Sending message: " + JSON.stringify(message));
         e.customRequest("updateInput", message.arguments);
       }
     });
   });
 
   vscode.debug.onDidTerminateDebugSession((e) => {
-    console.log("Debug session terminated");
+    output.appendLine("Debug session terminated");
     if (WebViewPanel.currentPanel) {
       WebViewPanel.currentPanel.dispose();
     }
   });
 
   vscode.debug.onDidReceiveDebugSessionCustomEvent((e) => {
-    console.log("Custom event received: ", e);
+    output.appendLine("Custom event received: " + JSON.stringify(e));
     if (e.event === "boardUpdate") {
-      console.log("Board update event");
+      output.appendLine("Board update event");
       if (WebViewPanel.currentPanel !== undefined) {
         WebViewPanel.currentPanel._panel.webview.postMessage({
           type: "boardUpdate",
@@ -52,13 +50,12 @@ export function activate(context: vscode.ExtensionContext) {
   activateMockDebug(context, new DebugAdapterExecutableFactory());
 }
 
-function showInformationMessage(message: any) { }
-
 // This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
 
 class DebugAdapterExecutableFactory
-  implements vscode.DebugAdapterDescriptorFactory {
+  implements vscode.DebugAdapterDescriptorFactory
+{
   // The following use of a DebugAdapter factory shows how to control what debug adapter executable is used.
   // Since the code implements the default behavior, it is absolutely not neccessary and we show it here only for educational purpose.
 
@@ -69,33 +66,50 @@ class DebugAdapterExecutableFactory
     // param "executable" contains the executable optionally specified in the package.json (if any)
 
     // Compile the program
-    console.log("Program: ", _session.configuration.program);
+    output.appendLine("Program: " + _session.configuration.program);
     await compileFile(_session.configuration.program);
-    
+    const dir = _session.configuration.program
+      .split("/")
+      .slice(0, -1)
+      .join("/");
+
+    // Check Vtb executable on the program folder
+    if (!executable) {
+      const executablePath = dir + "/Vtb";
+      output.appendLine("Checking: " + executablePath + " ");
+      if (existsSync(executablePath)) {
+        output.appendLine("Executable found: " + executablePath);
+        executable = new vscode.DebugAdapterExecutable(executablePath, [
+          _session.configuration.program,
+        ]);
+      }
+    }
     // Check Vtb executable on each workspace folder
     if (!executable) {
       for (const folder of vscode.workspace.workspaceFolders || []) {
         const executablePath = folder.uri.fsPath + "/Vtb";
-        if (require("fs").existsSync(executablePath)) {
-          executable = new vscode.DebugAdapterExecutable(
-            executablePath,
-            [_session.configuration.program]
-          );
+        output.appendLine("Checking: " + executablePath);
+        if (existsSync(executablePath)) {
+          output.appendLine("Executable found: " + executablePath);
+          executable = new vscode.DebugAdapterExecutable(executablePath, [
+            _session.configuration.program,
+          ]);
           break;
         }
-      } 
+      }
     }
     if (!executable) {
-      vscode.window.showErrorMessage("Vtb executable not found on the workspace folders");
+      vscode.window.showErrorMessage(
+        "Vtb executable not found on the workspace folders"
+      );
       return undefined;
     }
-    console.log("Executable: ", executable);
+    output.appendLine("Executable: " + JSON.stringify(executable));
 
     // make VS Code launch the DA executable
     return executable;
   }
 }
-
 
 function compileFile(filePath: string): Promise<void> {
   return new Promise((resolve, reject) => {
