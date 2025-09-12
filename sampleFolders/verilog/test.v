009ab6f3f61d6a3f9b562bf5da30edbfe95ea0ef
Bugra Eryilmaz <bugra.eryilmaz@epfl.ch>
2025-09-12 10:59:16 +0200

    fix signal names

--- sampleFolders/verilog/test.v
+++ sampleFolders/verilog/test.v
@@ -1,42 +1,42 @@
 module tb (
-        input        clk,
-        input        rst_n,
-        output reg [7:0] sevensegment_2,
-        output reg [119:0] led_r,
-        output reg [119:0] led_g,
-        input button_top,
-        input button_bottom,
-        input button_left,
-        input button_right,
-        input button_center,
-        input [7:0] dip_switches,
-        input joystick_up,
-        input joystick_down,
-        input joystick_left,
-        input joystick_right,
-        input joystick_pressed
+        input        clk_i,
+        input        rst_ni,
+        output reg [7:0] sevensegment_2_o,
+        output reg [119:0] led_r_o,
+        output reg [119:0] led_g_o,
+        input button_top_i,
+        input button_bottom_i,
+        input button_left_i,
+        input button_right_i,
+        input button_center_i,
+        input [7:0] dip_switches_i,
+        input joystick_up_i,
+        input joystick_down_i,
+        input joystick_left_i,
+        input joystick_right_i,
+        input joystick_pressed_i
     );
     
 
-    always @(posedge clk) begin
-        if (rst_n == 0) begin 
-            sevensegment_2 <= 0;  
-            led_r <= 0; 
-            led_g <= 0; 
-         end
+    always @(posedge clk_i) begin
+        if (rst_ni == 0) begin
+            sevensegment_2_o <= 0;
+            led_r_o <= 0;
+            led_g_o <= 0;
+        end
         else begin
-            led_g[7:0] <= dip_switches;
-            sevensegment_2[0] <= button_top;
-            sevensegment_2[1] <= button_bottom;
-            sevensegment_2[2] <= button_left;
-            sevensegment_2[3] <= button_right;
-            sevensegment_2[4] <= button_center;
-            led_r[13] <= joystick_up;
-            led_r[14] <= joystick_down;
-            led_r[15] <= joystick_left;
-            led_r[16] <= joystick_right;
-            led_r[5] <= joystick_pressed;
-            led_g[5] <= dip_switches[0];
+            led_g_o[7:0] <= dip_switches_i;
+            sevensegment_2_o[0] <= button_top_i;
+            sevensegment_2_o[1] <= button_bottom_i;
+            sevensegment_2_o[2] <= button_left_i;
+            sevensegment_2_o[3] <= button_right_i;
+            sevensegment_2_o[4] <= button_center_i;
+            led_r_o[13] <= joystick_up_i;
+            led_r_o[14] <= joystick_down_i;
+            led_r_o[15] <= joystick_left_i;
+            led_r_o[16] <= joystick_right_i;
+            led_r_o[5] <= joystick_pressed_i;
+            led_g_o[5] <= dip_switches_i[0];
 
         end 
     end
